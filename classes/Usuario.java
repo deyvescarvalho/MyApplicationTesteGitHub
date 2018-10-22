be6ee1bbfcec8636d6cbb561399b45f27f898917
Deyves Lima Carvalho <deyvescarvalho@gmail.com>
2018-10-22 18:04:54 -0300

    Initial commit

---
+++ classes/Usuario.java
@@ -0,0 +1,35 @@
+package com.example.deyves.myapplicationtestegithub.classes;
+
+public class Usuario {
+
+    private String nome;
+    private int idade;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
